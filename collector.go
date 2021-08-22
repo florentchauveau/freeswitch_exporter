2d29b3e7e87988e6829778c1e03a15111b7302e5
Florent CHAUVEAU <florent.chauveau@gmail.com>
2021-08-22 14:00:52 +0200

    chore: upgrade to go 1.17

--- collector.go
+++ collector.go
@@ -1,359 +1,363 @@
 package main
 
 import (
-    "bufio"
-    "encoding/json"
-    "fmt"
-    "io"
-    "log"
-    "net"
-    "net/textproto"
-    "net/url"
-    "regexp"
-    "strconv"
-    "sync"
-    "time"
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/textproto"
+	"net/url"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
 
-    "github.com/pkg/errors"
-    "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // Collector implements prometheus.Collector (see below).
 // it also contains the config of the exporter.
 type Collector struct {
-    URI      string
-    Timeout  time.Duration
-    Password string
+	URI      string
+	Timeout  time.Duration
+	Password string
 
-    conn  net.Conn
-    input *bufio.Reader
-    url   *url.URL
-    mutex sync.Mutex
+	conn  net.Conn
+	input *bufio.Reader
+	url   *url.URL
+	mutex sync.Mutex
 
-    up            prometheus.Gauge
-    failedScrapes prometheus.Counter
-    totalScrapes  prometheus.Counter
+	up            prometheus.Gauge
+	failedScrapes prometheus.Counter
+	totalScrapes  prometheus.Counter
 }
 
-// Metric are aither fetch from an api command, or from "status" parsing (thus RegexIndex)
+// Metric represents a prometheus metric. It is either fetched from an api command,
+// or from "status" parsing (thus the RegexIndex)
 type Metric struct {
-    Name       string
-    Help       string
-    Type       prometheus.ValueType
-    Command    string
-    RegexIndex int
+	Name       string
+	Help       string
+	Type       prometheus.ValueType
+	Command    string
+	RegexIndex int
 }
 
 const (
-    namespace = "freeswitch"
+	namespace = "freeswitch"
 )
 
 var (
-    metricList = []Metric{
-        Metric{Name: "current_calls", Type: prometheus.GaugeValue, Help: "Number of calls active", Command: "api show calls count as json"},
-        Metric{Name: "uptime_seconds", Type: prometheus.GaugeValue, Help: "Uptime in seconds", Command: "api uptime s"},
-        Metric{Name: "time_synced", Type: prometheus.GaugeValue, Help: "Is FreeSWITCH time in sync with exporter host time", Command: "api strepoch"},
-        Metric{Name: "sessions_total", Type: prometheus.CounterValue, Help: "Number of sessions since startup", RegexIndex: 1},
-        Metric{Name: "current_sessions", Type: prometheus.GaugeValue, Help: "Number of sessions active", RegexIndex: 2},
-        Metric{Name: "current_sessions_peak", Type: prometheus.GaugeValue, Help: "Peak sessions since startup", RegexIndex: 3},
-        Metric{Name: "current_sessions_peak_last_5min", Type: prometheus.GaugeValue, Help: "Peak sessions for the last 5 minutes", RegexIndex: 4},
-        Metric{Name: "current_sps", Type: prometheus.GaugeValue, Help: "Number of sessions per second", RegexIndex: 5},
-        Metric{Name: "current_sps_peak", Type: prometheus.GaugeValue, Help: "Peak sessions per second since startup", RegexIndex: 7},
-        Metric{Name: "current_sps_peak_last_5min", Type: prometheus.GaugeValue, Help: "Peak sessions per second for the last 5 minutes", RegexIndex: 8},
-        Metric{Name: "max_sps", Type: prometheus.GaugeValue, Help: "Max sessions per second allowed", RegexIndex: 6},
-        Metric{Name: "max_sessions", Type: prometheus.GaugeValue, Help: "Max sessions allowed", RegexIndex: 9},
-        Metric{Name: "current_idle_cpu", Type: prometheus.GaugeValue, Help: "CPU idle", RegexIndex: 11},
-        Metric{Name: "min_idle_cpu", Type: prometheus.GaugeValue, Help: "Minimum CPU idle", RegexIndex: 10},
-    }
-    statusRegex = regexp.MustCompile(`(\d+) session\(s\) since startup\s+(\d+) session\(s\) - peak (\d+), last 5min (\d+)\s+(\d+) session\(s\) per Sec out of max (\d+), peak (\d+), last 5min (\d+)\s+(\d+) session\(s\) max\s+min idle cpu (\d+\.\d+)\/(\d+\.\d+)`)
+	metricList = []Metric{
+		{Name: "current_calls", Type: prometheus.GaugeValue, Help: "Number of calls active", Command: "api show calls count as json"},
+		{Name: "uptime_seconds", Type: prometheus.GaugeValue, Help: "Uptime in seconds", Command: "api uptime s"},
+		{Name: "time_synced", Type: prometheus.GaugeValue, Help: "Is FreeSWITCH time in sync with exporter host time", Command: "api strepoch"},
+		{Name: "sessions_total", Type: prometheus.CounterValue, Help: "Number of sessions since startup", RegexIndex: 1},
+		{Name: "current_sessions", Type: prometheus.GaugeValue, Help: "Number of sessions active", RegexIndex: 2},
+		{Name: "current_sessions_peak", Type: prometheus.GaugeValue, Help: "Peak sessions since startup", RegexIndex: 3},
+		{Name: "current_sessions_peak_last_5min", Type: prometheus.GaugeValue, Help: "Peak sessions for the last 5 minutes", RegexIndex: 4},
+		{Name: "current_sps", Type: prometheus.GaugeValue, Help: "Number of sessions per second", RegexIndex: 5},
+		{Name: "current_sps_peak", Type: prometheus.GaugeValue, Help: "Peak sessions per second since startup", RegexIndex: 7},
+		{Name: "current_sps_peak_last_5min", Type: prometheus.GaugeValue, Help: "Peak sessions per second for the last 5 minutes", RegexIndex: 8},
+		{Name: "max_sps", Type: prometheus.GaugeValue, Help: "Max sessions per second allowed", RegexIndex: 6},
+		{Name: "max_sessions", Type: prometheus.GaugeValue, Help: "Max sessions allowed", RegexIndex: 9},
+		{Name: "current_idle_cpu", Type: prometheus.GaugeValue, Help: "CPU idle", RegexIndex: 11},
+		{Name: "min_idle_cpu", Type: prometheus.GaugeValue, Help: "Minimum CPU idle", RegexIndex: 10},
+	}
+	statusRegex = regexp.MustCompile(`(\d+) session\(s\) since startup\s+(\d+) session\(s\) - peak (\d+), last 5min (\d+)\s+(\d+) session\(s\) per Sec out of max (\d+), peak (\d+), last 5min (\d+)\s+(\d+) session\(s\) max\s+min idle cpu (\d+\.\d+)\/(\d+\.\d+)`)
 )
 
 // NewCollector processes uri, timeout and methods and returns a new Collector.
 func NewCollector(uri string, timeout time.Duration, password string) (*Collector, error) {
-    var c Collector
+	var c Collector
 
-    c.URI = uri
-    c.Timeout = timeout
-    c.Password = password
+	c.URI = uri
+	c.Timeout = timeout
+	c.Password = password
 
-    var url *url.URL
-    var err error
+	var url *url.URL
+	var err error
 
-    if url, err = url.Parse(c.URI); err != nil {
-        return nil, errors.Wrap(err, "cannot parse URI")
-    }
+	if url, err = url.Parse(c.URI); err != nil {
+		return nil, fmt.Errorf("cannot parse URI: %w", err)
+	}
 
-    c.url = url
+	c.url = url
 
-    c.up = prometheus.NewGauge(prometheus.GaugeOpts{
-        Namespace: namespace,
-        Name:      "up",
-        Help:      "Was the last scrape successful.",
-    })
+	c.up = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "up",
+		Help:      "Was the last scrape successful.",
+	})
 
-    c.totalScrapes = prometheus.NewCounter(prometheus.CounterOpts{
-        Namespace: namespace,
-        Name:      "exporter_total_scrapes",
-        Help:      "Current total freeswitch scrapes.",
-    })
+	c.totalScrapes = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "exporter_total_scrapes",
+		Help:      "Current total freeswitch scrapes.",
+	})
 
-    c.failedScrapes = prometheus.NewCounter(prometheus.CounterOpts{
-        Namespace: namespace,
-        Name:      "exporter_failed_scrapes",
-        Help:      "Number of failed freeswitch scrapes.",
-    })
+	c.failedScrapes = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "exporter_failed_scrapes",
+		Help:      "Number of failed freeswitch scrapes.",
+	})
 
-    return &c, nil
+	return &c, nil
 }
 
 // scrape will connect to the freeswitch instance and push metrics to the Prometheus channel.
 func (c *Collector) scrape(ch chan<- prometheus.Metric) error {
-    c.totalScrapes.Inc()
+	c.totalScrapes.Inc()
 
-    address := c.url.Host
+	address := c.url.Host
 
-    if c.url.Scheme == "unix" {
-        address = c.url.Path
-    }
+	if c.url.Scheme == "unix" {
+		address = c.url.Path
+	}
 
-    var err error
+	var err error
 
-    c.conn, err = net.DialTimeout(c.url.Scheme, address, c.Timeout)
+	c.conn, err = net.DialTimeout(c.url.Scheme, address, c.Timeout)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    c.conn.SetDeadline(time.Now().Add(c.Timeout))
-    defer c.conn.Close()
+	c.conn.SetDeadline(time.Now().Add(c.Timeout))
+	defer c.conn.Close()
 
-    c.input = bufio.NewReader(c.conn)
+	c.input = bufio.NewReader(c.conn)
 
-    if err = c.fsAuth(); err != nil {
-        return err
-    }
+	if err = c.fsAuth(); err != nil {
+		return err
+	}
 
-    if err = c.scapeMetrics(ch); err != nil {
-        return err
-    }
+	if err = c.scapeMetrics(ch); err != nil {
+		return err
+	}
 
-    if err = c.scrapeStatus(ch); err != nil {
-        return err
-    }
+	if err = c.scrapeStatus(ch); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func (c *Collector) scapeMetrics(ch chan<- prometheus.Metric) error {
-    for _, metricDef := range metricList {
-        if len(metricDef.Command) == 0 {
-            // this metric will be fetched by scapeStatus
-            continue
-        }
+	for _, metricDef := range metricList {
+		if len(metricDef.Command) == 0 {
+			// this metric will be fetched by scapeStatus
+			continue
+		}
 
-        value, err := c.fetchMetric(&metricDef)
+		value, err := c.fetchMetric(&metricDef)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        metric, err := prometheus.NewConstMetric(
-            prometheus.NewDesc(namespace+"_"+metricDef.Name, metricDef.Help, nil, nil),
-            metricDef.Type,
-            value,
-        )
+		metric, err := prometheus.NewConstMetric(
+			prometheus.NewDesc(namespace+"_"+metricDef.Name, metricDef.Help, nil, nil),
+			metricDef.Type,
+			value,
+		)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        ch <- metric
-    }
+		ch <- metric
+	}
 
-    return nil
+	return nil
 }
 
 func (c *Collector) scrapeStatus(ch chan<- prometheus.Metric) error {
-    response, err := c.fsCommand("api status")
+	response, err := c.fsCommand("api status")
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    matches := statusRegex.FindAllStringSubmatch(string(response), -1)
+	matches := statusRegex.FindAllSubmatch(response, -1)
 
-    if len(matches) != 1 {
-        return errors.New("error parsing status")
-    }
+	if len(matches) != 1 {
+		return errors.New("error parsing status")
+	}
 
-    for _, metricDef := range metricList {
-        if len(metricDef.Command) != 0 {
-            // this metric will be fetched by fetchMetric
-            continue
-        }
+	for _, metricDef := range metricList {
+		if len(metricDef.Command) != 0 {
+			// this metric will be fetched by fetchMetric
+			continue
+		}
 
-        if len(matches[0]) < metricDef.RegexIndex {
-            return errors.New("error parsing status")
-        }
+		if len(matches[0]) < metricDef.RegexIndex {
+			return errors.New("error parsing status")
+		}
 
-        strValue := matches[0][metricDef.RegexIndex]
-        value, err := strconv.ParseFloat(strValue, 64)
+		strValue := string(matches[0][metricDef.RegexIndex])
+		value, err := strconv.ParseFloat(strValue, 64)
 
-        if err != nil {
-            return errors.Wrap(err, "error parsing status")
-        }
+		if err != nil {
+			return fmt.Errorf("error parsing status: %w", err)
+		}
 
-        metric, err := prometheus.NewConstMetric(
-            prometheus.NewDesc(namespace+"_"+metricDef.Name, metricDef.Help, nil, nil),
-            metricDef.Type,
-            value,
-        )
+		metric, err := prometheus.NewConstMetric(
+			prometheus.NewDesc(namespace+"_"+metricDef.Name, metricDef.Help, nil, nil),
+			metricDef.Type,
+			value,
+		)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        ch <- metric
-    }
+		ch <- metric
+	}
 
-    return nil
+	return nil
 }
 
 func (c *Collector) fetchMetric(metricDef *Metric) (float64, error) {
-    now := time.Now()
-    response, err := c.fsCommand(metricDef.Command)
+	now := time.Now()
+	response, err := c.fsCommand(metricDef.Command)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    switch metricDef.Name {
-    case "current_calls":
-        r := struct {
-            Count float64 `json:"row_count"`
-        }{}
+	switch metricDef.Name {
+	case "current_calls":
+		r := struct {
+			Count float64 `json:"row_count"`
+		}{}
 
-        err = json.Unmarshal(response, &r)
+		err = json.Unmarshal(response, &r)
 
-        if err != nil {
-            return 0, errors.Wrap(err, "cannot read JSON response")
-        }
+		if err != nil {
+			return 0, fmt.Errorf("cannot read JSON response: %w", err)
+		}
 
-        return r.Count, nil
-    case "uptime_seconds":
-        raw := string(response)
+		return r.Count, nil
+	case "uptime_seconds":
+		raw := string(response)
 
-        if raw[len(raw)-1:len(raw)] == "\n" {
-            raw = raw[:len(raw)-1]
-        }
+		if raw[len(raw)-1:] == "\n" {
+			raw = raw[:len(raw)-1]
+		}
 
-        value, err := strconv.ParseFloat(raw, 64)
+		value, err := strconv.ParseFloat(raw, 64)
 
-        if err != nil {
-            return 0, errors.Wrap(err, "cannot read uptime")
-        }
+		if err != nil {
+			return 0, fmt.Errorf("cannot read uptime: %w", err)
+		}
 
-        return value, nil
-    case "time_synced":
-        value, err := strconv.ParseInt(string(response), 10, 64)
+		return value, nil
+	case "time_synced":
+		value, err := strconv.ParseInt(string(response), 10, 64)
 
-        if err != nil {
-            return 0, errors.Wrap(err, "cannot read FreeSWITCH time")
-        }
+		if err != nil {
+			return 0, fmt.Errorf("cannot read FreeSWITCH time: %w", err)
+		}
 
-        if now.Unix() == value {
-            return 1, nil
-        }
+		if now.Unix() == value {
+			return 1, nil
+		}
 
-        return 0, nil
-    }
+		log.Printf("[warning] time not in sync between system (%v) and FreeSWITCH (%v)\n",
+			now.Unix(), value)
 
-    return 0, errors.Errorf("unknown metric: %s", metricDef.Name)
+		return 0, nil
+	}
+
+	return 0, fmt.Errorf("unknown metric: %s", metricDef.Name)
 }
 
 func (c *Collector) fsCommand(command string) ([]byte, error) {
-    _, err := c.conn.Write([]byte(command + "\n\n"))
+	_, err := io.WriteString(c.conn, command+"\n\n")
 
-    if err != nil {
-        return nil, errors.Wrap(err, "cannot write command")
-    }
+	if err != nil {
+		return nil, fmt.Errorf("cannot write command: %w", err)
+	}
 
-    mimeReader := textproto.NewReader(c.input)
-    message, err := mimeReader.ReadMIMEHeader()
+	mimeReader := textproto.NewReader(c.input)
+	message, err := mimeReader.ReadMIMEHeader()
 
-    if err != nil {
-        return nil, errors.Wrap(err, "cannot read command response")
-    }
+	if err != nil {
+		return nil, fmt.Errorf("cannot read command response: %w", err)
+	}
 
-    value := message.Get("Content-Length")
-    length, _ := strconv.Atoi(value)
+	value := message.Get("Content-Length")
+	length, _ := strconv.Atoi(value)
 
-    body := make([]byte, length)
-    _, err = io.ReadFull(c.input, body)
+	body := make([]byte, length)
+	_, err = io.ReadFull(c.input, body)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return body, nil
+	return body, nil
 }
 
 func (c *Collector) fsAuth() error {
-    mimeReader := textproto.NewReader(c.input)
-    message, err := mimeReader.ReadMIMEHeader()
+	mimeReader := textproto.NewReader(c.input)
+	message, err := mimeReader.ReadMIMEHeader()
 
-    if err != nil {
-        return errors.Wrap(err, "read auth failed")
-    }
+	if err != nil {
+		return fmt.Errorf("read auth failed: %w", err)
+	}
 
-    if message.Get("Content-Type") != "auth/request" {
-        return errors.New("auth failed (unknown content-type)")
-    }
+	if message.Get("Content-Type") != "auth/request" {
+		return errors.New("auth failed: unknown content-type")
+	}
 
-    _, err = c.conn.Write([]byte(fmt.Sprintf("auth %s\n\n", c.Password)))
+	_, err = io.WriteString(c.conn, fmt.Sprintf("auth %s\n\n", c.Password))
 
-    if err != nil {
-        return errors.Wrap(err, "write auth failed")
-    }
+	if err != nil {
+		return fmt.Errorf("write auth failed: %w", err)
+	}
 
-    message, err = mimeReader.ReadMIMEHeader()
+	message, err = mimeReader.ReadMIMEHeader()
 
-    if err != nil {
-        return errors.Wrap(err, "read auth failed")
-    }
+	if err != nil {
+		return fmt.Errorf("read auth failed: %w", err)
+	}
 
-    if message.Get("Content-Type") != "command/reply" {
-        return errors.New("auth failed (unknown reply)")
-    }
+	if message.Get("Content-Type") != "command/reply" {
+		return errors.New("auth failed: unknown reply")
+	}
 
-    if message.Get("Reply-Text") != "+OK accepted" {
-        return errors.Errorf("auth failed: %s", message.Get("Reply-Text"))
-    }
+	if message.Get("Reply-Text") != "+OK accepted" {
+		return fmt.Errorf("auth failed: %s", message.Get("Reply-Text"))
+	}
 
-    return nil
+	return nil
 }
 
 // Describe implements prometheus.Collector.
 func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
-    prometheus.DescribeByCollect(c, ch)
+	prometheus.DescribeByCollect(c, ch)
 }
 
 // Collect implements prometheus.Collector.
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
-    c.mutex.Lock()
-    defer c.mutex.Unlock()
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 
-    err := c.scrape(ch)
+	err := c.scrape(ch)
 
-    if err != nil {
-        c.failedScrapes.Inc()
-        c.up.Set(0)
-        log.Println("[error]", err)
-    } else {
-        c.up.Set(1)
-    }
+	if err != nil {
+		c.failedScrapes.Inc()
+		c.up.Set(0)
+		log.Println("[error]", err)
+	} else {
+		c.up.Set(1)
+	}
 
-    ch <- c.up
-    ch <- c.totalScrapes
-    ch <- c.failedScrapes
+	ch <- c.up
+	ch <- c.totalScrapes
+	ch <- c.failedScrapes
 }
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/florentchauveau/freeswitch_exporter
+
+go 1.17
+
+require (
+	github.com/prometheus/client_golang v1.11.0
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
